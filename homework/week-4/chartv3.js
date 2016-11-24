628a3a5515d01f6fc7487868971c7c421bcfec28
evavanzelm <evavanzelm@hotmail.com>
2016-11-24 17:20:20 +0100

    graph version 1

--- homework/week-4/chartv3.js
+++ homework/week-4/chartv3.js
@@ -1,67 +1,110 @@
-// //set margins
-// var margin = {top: 20, right: 30, bottom: 30, left: 40},
-//     width = 960 - margin.left - margin.right,
-//     height = 500 - margin.top - margin.bottom;
-
-
-var width = 960,
-    height = 500;
+//set margins
+var margin = {top: 20, right: 50, bottom: 140, left: 80},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
 //set ordinal scale
 var x = d3.scale.ordinal()
     .rangeRoundBands([0, width], .1);
 
 // set linear y scale
-var y = d3.scale.log()
+var y = d3.scale.linear()
     .range([height, 0]);
 
+// create axis variables
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+
+var tip = d3.tip()
+  .attr('class', 'd3-tip')
+  .offset([-10, 0])
+  .html(function(d) {
+    return "<strong>Emission:</strong> <span style='color:#b10026'>" + d.Data + "</span>";
+  })
+
 var chart = d3.select(".chart")
-    .attr("width", width)
-    .attr("height", height);
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// // create axis variables
-// var xAxis = d3.svg.axis()
-//     .scale(x)
-//     .orient("bottom");
+chart.call(tip);
 
+//Make an SVG Container
+var svgContainer = d3.select(".squere")
+  .attr("width", 200)
+  .attr("height", 200)
+  .style("visibility", "hidden");
 
-// var yAxis = d3.svg.axis()
-//     .scale(y)
-//     .orient("left")
-//     .ticks(10, "%");
-
-// var chart = d3.select(".chart")
-//     .attr("width", width + margin.left + margin.right)
-//     .attr("height", height + margin.top + margin.bottom)
-//   .append("g")
-//     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
+ 
+ //Draw the Rectangle
+var rectangle = svgContainer.append("squere")
+  .attr("x", 10)
+  .attr("y", 10)
+  .attr("width", 50)
+  .attr("height", 100);
+  
 
 // reading in data
-d3.json("data.json", function(error, data) {
+d3.json("data.json", function(data) {
 
-	for (var i = 0; i < data.length; i++){
-		data[i].Data = +data[i].Data;
-	}
+  for (var i = 0; i < data.length; i++){
+    data[i].Data = +Math.round(data[i].Data);
+  }
 
-	console.log(data);
+  console.log(data);
   x.domain(data.map(function(d) { return d.Country; }));
   y.domain([0, d3.max(data, function(d) { return d.Data; })]);
 
-   
-  var bar = chart.selectAll("g")
-      .data(data)
-    .enter().append("g")
-      .attr("transform", function(d) { return "translate(" + x(d.Country) + ",0)"; });
+  chart.append("g")
+     .attr("class", "x axis")
+     .attr("transform", "translate(0," + height + ")")
+     .call(xAxis)
+     .selectAll("text") 
+            .style("text-anchor", "end")
+            .attr("dx", "-.8em")
+            .attr("dy", ".15em")
+            .attr("transform", function(d) {
+                return "rotate(-65)" 
+                });
 
-  bar.append("rect")
-      .attr("y", function(d) { return y(d.Data); })
-      .attr("height", function(d) { return height - y(d.Data); })
-      .attr("width", x.rangeBand());
+  chart.append("g")
+    .attr("class", "y axis")
+    .call(yAxis)
+  .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", - 60)
 
-  bar.append("text")
-      .attr("x", x.rangeBand() / 2)
-      .attr("y", function(d) { return y(d.Data) + 3; })
-      .attr("dy", ".75em")
-      .text(function(d) { return d.Data; });
-});
+    .style("text-anchor", "end")
+    .text("Average anual carbon emmission (million metric tons)");
+
+  chart.selectAll(".bar")
+    .data(data)
+  .enter().append("rect")
+    .attr("class", "bar")
+    .attr("x", function(d) { return x(d.Country); })
+    .attr("y", function(d) { return y((d.Data)); })
+    .attr("height", function(d) { return height - y((d.Data)); })
+    .attr("width", x.rangeBand())
+    // .on('mouseover', tip.show)
+    // .on('mouseout', tip.hide)
+    .on("mouseover",  function(d) {
+      tip.show(d)
+      d3.select(this).attr("r", 10).style("fill", "#b10026");
+      d3.select(".squere").style("visibility", "visible");
+    })                  
+    .on("mouseout", function(d) {
+       tip.hide(d)
+      d3.select(this).attr("r", 5.5).style("fill", "#1c9099");
+      d3.select(".squere").style("visibility", "hidden");
+    });
+
+
+  });
+
+
